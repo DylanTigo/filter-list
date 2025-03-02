75ff939026289ec07be377427bc27939c2f8dbb8
DylanTigo <dylannoeltigomo@gmail.com>
2025-03-02 08:21:12 +0100

    Fix style on container

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CathegoriesList from "@/components/CathegoriesList";
 
 export default function Home() {
   return (
-    <main className="flex justify-center mt-44 w-screen h-screen overflow-x-hidden">
+    <main className="flex justify-center mt-44 w-screen overflow-x-hidden">
       <CathegoriesList/>
     </main>
   );
